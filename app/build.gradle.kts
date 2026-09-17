3915321b49516c423aa106877e909cc2e861d84d
space-shell <j-n@hotmail.co.uk>
2026-09-17 13:30:00 +0200

    release 0.3.3: return-to-send key, version footer, installability fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.jamesnicholls.nemotronvoice"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.3.2"
+        versionCode = 24
+        versionName = "0.3.3"
         ndk {
             abiFilters += "arm64-v8a"
         }
