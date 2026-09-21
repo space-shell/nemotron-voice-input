927f23507a15e36bcea0bfc3ea9193a326fb4e94
space-shell <j-n@hotmail.co.uk>
2026-09-21 19:53:13 +0200

    release 0.4.1: recognizer cold-start hang mitigations (#16)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.jamesnicholls.nemotronvoice"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.4.0"
+        versionCode = 26
+        versionName = "0.4.1"
         ndk {
             abiFilters += "arm64-v8a"
         }
